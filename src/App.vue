6c38a9840ba682adb326ead1029e22dd952149d9
Toki <1271297639@qq.com>
2023-02-07 13:08:22 +0800

    feat(package.json): added a new package
    added the "vue-dompurify-html" to take the place of  the "v-html"

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template><router-view /></template>
 
 <script lang="ts" setup="setup">
-//引入animate.css动画库
-// import "animate.css/animate.min.css";
 </script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from "vue";
 import { createHead } from "@vueuse/head";
 import { router } from "@/router";
 import { createPinia } from "pinia";
+
+//代替v-html
+import VueDOMPurifyHTML from "vue-dompurify-html";
+
 import App from "./App.vue";
 import "./index.css";
 
@@ -12,4 +16,5 @@ const pinia = createPinia();
 app.use(head);
 app.use(router);
 app.use(pinia);
+app.use(VueDOMPurifyHTML);
 app.mount("#app");
